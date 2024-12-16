47098d4d1b4fe1a68de89db2f19ac5d1034caef4
Aguayo <gjaguayo@cps.k12.il.us>
2024-12-16 11:50:09 -0600

    new changes

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import streamlit as st
-st.write("Hello, World!")
 
-st.title("Title")
-st.header("Header")
-st.subheader("Sub-header")
+# Set the title of the web page
+st.title("Hancock Wrestling Team Confrence Mega Duel!")
+
+# Set a header
+st.header("Enter who you think will win")
+# Text input for name
+winner = st.text_input("")
+# Dropdown menu for selecting an option
+options = ["Simeon HS, Bogan HS, Englewood Stem HS, Solorio Academy HS, George Washington HS, Hubbard HS, Hancock HS."]
+# Slider for selecting a value
+
+# Submit button
+
+# Additional information
+
+# Footer
+
 st.button("Edging Button")
